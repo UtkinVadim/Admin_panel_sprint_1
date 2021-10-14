f1a010691b125dc5ba9707ba5cce15eb9de6adf4
vadim <vadim@MacBook-Pro.local>
2021-10-14 23:42:55 +0300

    add writer class

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,14 +4,30 @@ import sqlite3
 import datetime
 import psycopg2
 
+from typing import List
 from dataclasses import dataclass
 from psycopg2.extras import DictCursor
 from psycopg2.extensions import connection as _connection
 
+DELIMITER = '|'
+
 
 @dataclass(frozen=True)
-class FilmWork:
+class AbsDataclass:
     id: uuid
+
+    def fields(self) -> tuple:
+        raise NotImplementedError
+
+    def field_names(self) -> list:
+        raise NotImplementedError
+
+    def data_to_write(self) -> str:
+        return DELIMITER.join(self.fields()) + "\n"
+
+
+@dataclass(frozen=True)
+class FilmWork(AbsDataclass):
     title: str
     description: str
     creation_date: datetime
@@ -21,6 +37,7 @@ class FilmWork:
     type: str
     created_at: datetime
     updated_at: datetime
+    table_name: str = 'film_work'
 
     def fields(self) -> tuple:
         fields = (
@@ -37,16 +54,28 @@ class FilmWork:
         )
         return fields
 
+    def field_names(self) -> list:
+        return ['id',
+                'title',
+                'description',
+                'creation_date',
+                'certificate',
+                'file_path',
+                'rating',
+                'type',
+                'created_at',
+                'updated_at']
+
 
 @dataclass(frozen=True)
-class Genre:
-    id: uuid
+class Genre(AbsDataclass):
     name: str
     description: str
     created_at: datetime
     updated_at: datetime
+    table_name: str = 'genre'
 
-    def fields(self):
+    def fields(self) -> tuple:
         fields = (
             str(self.id),
             str(self.name),
@@ -56,14 +85,17 @@ class Genre:
         )
         return fields
 
+    def field_names(self) -> list:
+        return ['id', 'name', 'description', 'created_at', 'updated_at']
+
 
 @dataclass(frozen=True)
-class Person:
-    id: uuid
+class Person(AbsDataclass):
     full_name: str
     birth_date: datetime
     created_at: datetime
     updated_at: datetime
+    table_name: str = 'person'
 
     def fields(self):
         fields = (
@@ -75,13 +107,16 @@ class Person:
         )
         return fields
 
+    def field_names(self) -> list:
+        return ['id', 'full_name', 'birth_date', 'created_at', 'updated_at']
+
 
 @dataclass(frozen=True)
-class GenreFilmWork:
-    id: uuid
+class GenreFilmWork(AbsDataclass):
     film_work_id: uuid
     genre_id: uuid
     created_at: datetime
+    table_name: str = 'genre_film_work'
 
     def fields(self):
         fields = (
@@ -92,14 +127,17 @@ class GenreFilmWork:
         )
         return fields
 
+    def field_names(self) -> list:
+        return ['id', 'film_work_id', 'genre_id', 'created_at']
+
 
 @dataclass(frozen=True)
-class PersonFilmWork:
-    id: uuid
+class PersonFilmWork(AbsDataclass):
     film_work_id: uuid
     person_id: uuid
     role: str
     created_at: datetime
+    table_name: str = 'person_film_work'
 
     def fields(self):
         fields = (
@@ -111,207 +149,99 @@ class PersonFilmWork:
         )
         return fields
 
+    def field_names(self) -> list:
+        return ['id', 'film_work_id', 'person_id', 'role', 'created_at']
+
 
 class PostgresSaver:
     def __init__(self, pg_conn: _connection, data: dict):
         self.connect: _connection = pg_conn
         self.data = data
-        self.delimiter = '|'
+        self.cursor: DictCursor = self.connect.cursor()
+        self.insert_limit: int = 200
 
-    def save_all_data(self):
-        print("\nStart import!")
-        print("-------------------------")
-        start_import_time = datetime.datetime.now()
-        with self.connect.cursor() as cursor:
-            cursor.execute(open('../schema_design/db_schema.sql', 'r').read())
-            self._save_film_work(cursor)
-            self._save_genre(cursor)
-            self._save_person(cursor)
-            self._save_genre_film_work(cursor)
-            self._save_person_film_work(cursor)
-        print("-------------------------")
-        print(f"All tables imported successfully. During time: {datetime.datetime.now() - start_import_time}")
+    def save_all_data(self) -> None:
+        self.cursor.execute(open('../schema_design/db_schema.sql', 'r').read())
+        for table_name, tables in self.data.items():
+            self.cursor.execute(f'TRUNCATE {table_name}')
+            self._write_data_from_tables(tables)
+            self._check_load_data(tables)
+        self.cursor.close()
 
-    def _save_film_work(self, cursor: DictCursor) -> None:
-        table_name = 'film_work'
-        cursor.execute('TRUNCATE film_work')
-        table_data = self._get_table_data(table_name=table_name)
-        for film_work in table_data:
-            data = io.StringIO()
-            film_work_data = self.delimiter.join(film_work.fields())
-            data.write(film_work_data)
+    def _write_data_from_tables(self, tables: List[dataclass]) -> None:
+        data = io.StringIO()
+        table_instance = tables[0]
+        data_count = 0
+        for table in tables:
+            data_count += 1
+            data.write(table.data_to_write())
+            if data_count == self.insert_limit:
+                data = self._copy(data, table_instance)
+                data_count = 0
+
+        self._copy(data, table_instance)
+
+    def _copy(self, data: io.StringIO, table: dataclass) -> io.StringIO:
+        try:
             data.seek(0)
-            cursor.copy_from(file=data,
-                             table=table_name,
-                             sep=self.delimiter,
-                             null='null',
-                             columns=['id', 'title', 'description', 'creation_date',
-                                      'certificate', 'file_path', 'rating', 'type',
-                                      'created_at', 'updated_at'])
+            self.cursor.copy_from(file=data,
+                                  table=table.table_name,
+                                  sep=DELIMITER,
+                                  null='null',
+                                  columns=table.field_names())
+        except psycopg2.Error as err:
+            raise ValueError(f'Writing error: {err.pgerror}')
+        else:
+            return io.StringIO()
 
-            cursor.execute("""SELECT title FROM content.film_work WHERE id = '%s'""" % film_work.id)
-            result = cursor.fetchone()
-            assert str(result)[2:-2] == film_work.title
-
-        cursor.execute("""SELECT count(*) FROM content.film_work""")
-        result = cursor.fetchone()
-        assert str(result).strip('[]') == str(len(table_data))
-        print("Film work saving success!\n")
-
-    def _save_genre(self, cursor: DictCursor) -> None:
-        table_name = 'genre'
-        cursor.execute("""TRUNCATE genre""")
-        table_data = self._get_table_data(table_name=table_name)
-        for genre in table_data:
-            data = io.StringIO()
-            genre_data = self.delimiter.join(genre.fields())
-            data.write(genre_data)
-            data.seek(0)
-            cursor.copy_from(file=data,
-                             table=table_name,
-                             sep=self.delimiter,
-                             null='null',
-                             columns=['id', 'name', 'description', 'created_at', 'updated_at'])
-
-            cursor.execute("""SELECT name FROM content.genre WHERE id = '%s'""" % genre.id)
-            result = cursor.fetchone()
-            assert str(result)[2:-2] == genre.name
-
-        cursor.execute("""SELECT count(*) FROM content.genre""")
-        result = cursor.fetchone()
-        assert str(result).strip('[]') == str(len(table_data))
-        print("Genre saving success!\n")
-
-    def _save_person(self, cursor: DictCursor) -> None:
-        table_name = 'person'
-        cursor.execute("""TRUNCATE person""")
-        table_data = self._get_table_data(table_name=table_name)
-        for person in table_data:
-            data = io.StringIO()
-            person_data = self.delimiter.join(person.fields())
-            data.write(person_data)
-            data.seek(0)
-            cursor.copy_from(file=data,
-                             table=table_name,
-                             sep=self.delimiter,
-                             null='null',
-                             columns=['id', 'full_name', 'birth_date', 'created_at', 'updated_at'])
-
-            cursor.execute("""SELECT full_name FROM content.person WHERE id = '%s'""" % person.id)
-            result = cursor.fetchone()
-            assert str(result)[2:-2] == person.full_name
-
-        cursor.execute("""SELECT count(*) FROM content.person""")
-        result = cursor.fetchone()
-        assert str(result).strip('[]') == str(len(table_data))
-        print("Person saving success!\n")
-
-    def _save_genre_film_work(self, cursor: DictCursor) -> None:
-        table_name = 'genre_film_work'
-        cursor.execute("""TRUNCATE genre_film_work""")
-        table_data = self._get_table_data(table_name=table_name)
-        for genre_film_work in table_data:
-            data = io.StringIO()
-            genre_film_work_data = self.delimiter.join(genre_film_work.fields())
-            data.write(genre_film_work_data)
-            data.seek(0)
-            cursor.copy_from(file=data,
-                             table=table_name,
-                             sep=self.delimiter,
-                             null='null',
-                             columns=['id', 'film_work_id', 'genre_id', 'created_at'])
-
-            cursor.execute("""SELECT film_work_id FROM content.genre_film_work WHERE id = '%s'""" % genre_film_work.id)
-            result = cursor.fetchone()
-            assert str(result)[2:-2] == genre_film_work.film_work_id
-
-        cursor.execute("""SELECT count(*) FROM content.genre_film_work""")
-        result = cursor.fetchone()
-        assert str(result).strip('[]') == str(len(table_data))
-        print("Genre - Film Work saving success!\n")
-
-    def _save_person_film_work(self, cursor: DictCursor) -> None:
-        table_name = 'person_film_work'
-        cursor.execute("""TRUNCATE person_film_work""")
-        table_data = self._get_table_data(table_name=table_name)
-        for person_film_work in table_data:
-            data = io.StringIO()
-            person_film_work_data = self.delimiter.join(person_film_work.fields())
-            data.write(person_film_work_data)
-            data.seek(0)
-            cursor.copy_from(file=data,
-                             table=table_name,
-                             sep=self.delimiter,
-                             null='null',
-                             columns=['id', 'film_work_id', 'person_id', 'role', 'created_at'])
-
-            cursor.execute("""SELECT person_id FROM content.person_film_work WHERE id = '%s'""" % person_film_work.id)
-            result = cursor.fetchone()
-            assert str(result)[2:-2] == person_film_work.person_id
-
-        cursor.execute("""SELECT count(*) FROM content.person_film_work""")
-        result = cursor.fetchone()
-        assert str(result).strip('[]') == str(len(table_data))
-        print("Person - Film Work saving success!\n")
-
-    def _get_table_data(self, table_name: str) -> list:
-        return self.data[table_name]
+    def _check_load_data(self, tables: list) -> None:
+        table_instance = tables[0]
+        self.cursor.execute(f'SELECT count(*) FROM content.{table_instance.table_name}')
+        result = self.cursor.fetchone()
+        assert str(result).strip('[]') == str(len(tables))
+        print(f"{table_instance.table_name.replace('_', ' ').title()} saving success!\n")
 
 
 class SQLiteLoader:
     def __init__(self, connection: sqlite3.Connection):
-        self.connection = connection
+        self.connection: sqlite3.Connection = connection
 
     @property
-    def cur(self):
+    def cur(self) -> sqlite3.Cursor:
         return self.connection.cursor()
 
-    def load_movies(self) -> dict:
-        film_works = self._get_film_works_data()
-        genres = self._get_genre_data()
-        persons = self._get_persons_data()
-        genre_film_works = self._get_genre_film_works_data()
-        person_film_works = self._get_person_film_works_data()
-
-        data = {
-            "film_work": film_works,
-            "genre": genres,
-            "person": persons,
-            "genre_film_work": genre_film_works,
-            "person_film_work": person_film_works
+    @property
+    def _table_dataclass_handler(self) -> dict:
+        return {
+            "film_work": FilmWork,
+            "genre": Genre,
+            "person": Person,
+            "genre_film_work": GenreFilmWork,
+            "person_film_work": PersonFilmWork
         }
-        self.cur.close()
-        return data
 
-    def _get_film_works_data(self) -> list:
-        film_works = []
-        for row in self.cur.execute(f'SELECT * FROM film_work'):
-            film_works.append(FilmWork(*row))
-        return film_works
+    def load_movies(self) -> dict:
+        try:
+            data = {
+                "film_work": self._get_data_from_table(table_name="film_work"),
+                "genre": self._get_data_from_table(table_name="genre"),
+                "person": self._get_data_from_table(table_name="person"),
+                "genre_film_work": self._get_data_from_table(table_name="genre_film_work"),
+                "person_film_work": self._get_data_from_table(table_name="person_film_work"),
+            }
+        except sqlite3.OperationalError as err:
+            raise ValueError(f'Read error: {err}')
+        else:
+            return data
+        finally:
+            self.cur.close()
 
-    def _get_genre_data(self) -> list:
-        genres = []
-        for row in self.cur.execute(f'SELECT * FROM genre'):
-            genres.append(Genre(*row))
-        return genres
-
-    def _get_persons_data(self):
-        persons = []
-        for row in self.cur.execute(f'SELECT * FROM person'):
-            persons.append(Person(*row))
-        return persons
-
-    def _get_genre_film_works_data(self):
-        genre_film_works = []
-        for row in self.cur.execute(f'SELECT * FROM genre_film_work'):
-            genre_film_works.append(GenreFilmWork(*row))
-        return genre_film_works
-
-    def _get_person_film_works_data(self):
-        person_film_works = []
-        for row in self.cur.execute(f'SELECT * FROM person_film_work'):
-            person_film_works.append(PersonFilmWork(*row))
-        return person_film_works
+    def _get_data_from_table(self, table_name: str) -> dataclass:
+        tables = []
+        table_dataclass = self._table_dataclass_handler[table_name]
+        for row in self.cur.execute(f'SELECT * FROM {table_name}'):
+            tables.append(table_dataclass(*row))
+        return tables
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
